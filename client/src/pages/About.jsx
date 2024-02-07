936a8b1519a8e22cc3a273225f9348f6a0e98556
Aminejebara <aminebenjebara5@gamil.com>
2024-02-07 20:22:10 +0100

    oooooooooooooouh

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./about.css";
+export default function About() {
+  return (
+    <div className="py-20  ">
+      <div className="backgournd"></div>
+
+      <h1 className="text-3xl font-bold mb-4 text-center text-pink-400 pt-10">
+        About HOMELET
+      </h1>
+      <p className="mb-4 text-center text-slate-700 ">
+        HOMELET is a leading real estate agency that specializes in helping
+        clients buy, sell, and rent properties in the most desirable
+        neighborhoods. Our team of experienced agents is dedicated to providing
+        exceptional service and making the buying and selling process as smooth
+        as possible.
+      </p>
+      <p className="mb-4 text-center text-slate-700">
+        Our mission is to help our clients achieve their real estate goals by
+        providing expert advice, personalized service, and a deep understanding
+        of the local market. Whether you are looking to buy, sell, or rent a
+        property, we are here to help you every step of the way.
+      </p>
+      <p className="mb-4 text-center text-slate-700 ">
+        Our team of agents has a wealth of experience and knowledge in the real
+        estate industry, and we are committed to providing the highest level of
+        service to our clients. We believe that buying or selling a property
+        should be an exciting and rewarding experience, and we are dedicated to
+        making that a reality for each and every one of our clients.
+      </p>
+
+      <h1 className="text-3xl font-bold mb-4 text-center text-pink-400 pt-12  ">
+        Meet Our Team
+      </h1>
+      <div className="center pb-10">
+        <img
+          className="ima"
+          src="https://scontent.ftun1-2.fna.fbcdn.net/v/t1.15752-9/362505224_3021154491350568_1620808307017133736_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=hi-x0Dvoa2YAX-uAwvR&_nc_ht=scontent.ftun1-2.fna&oh=03_AdTVPzWWcAMJTjl7zOqHuw2NC0-TKDcTzBqLufAXDLMTTg&oe=65B59819"
+        ></img>
+        <img
+          className="ima"
+          src="https://scontent.ftun1-2.fna.fbcdn.net/v/t39.30808-6/315729877_1392579797815690_2427584342947092508_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=3hNcr0GyXHMAX_Y4Zp4&_nc_ht=scontent.ftun1-2.fna&oh=00_AfBpVdFo5zpuWGNewaCCVr1ccFzfNZpVRd2lnO9WWg8IaA&oe=659381AE"
+        ></img>
+      </div>
+
+      <p className="mb-4 text-center text-slate-700  pr-4 pl-20  font-semibold">
+        Introducing Amine Ben Jebara and Yasmine Ben Abda, dynamic computer
+        science students from the esteemed University of FST. Together, they
+        have harnessed their passion for technology and real estate to create a
+        cutting-edge estate agency website. With a solid foundation in computer
+        science, their innovative approach combines technical expertise with a
+        keen unde
+      </p>
+      <p className="mb-4 text-center text-slate-700 pt-25 pl-20 pr-20  font-semibold">
+        Amine and Yasmine's commitment to excellence is evident in every as pect
+        of their website, where they seamlessly merge their academic knowledge
+        with practical skills. Their journey at the University of FST has
+        equipped them with a robust understanding of programming languages,
+        software development, and database management — skills that form the
+        backbone of their sophisticated estate agency platform.
+      </p>
+      <p className="mb-4 text-center text-slate-700 pt-25 pl-20 pr-20  pr-4  font-semibold">
+        Driven by a shared vision, Amine and Yasmine have crafted a
+        user-friendly website that not only showcases their technical prowess
+        but also caters to the specific needs of clients in the real estate
+        market. Their dedication to creating an immersive online experience sets
+        their project apart, providing users with a seamless interface to
+        explore and engage with property listings.
+      </p>
+    </div>
+  );
+}
